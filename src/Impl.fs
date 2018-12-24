fb02759447773326ddb66ea81aadd1b747c8f8d1
cappellinsamuele <40738231+cappellinsamuele@users.noreply.github.com>
2018-12-24 14:38:02 +0100

    Versione 2.3
    Aggiornamento

---
+++ src/Impl.fs
@@ -0,0 +1,43 @@
+﻿
+(*
+ * SharpSolver - Progetto di Programmazione e Calcolo a.a. 2018-19
+ * Impl.fsi: implementazioni degli studenti
+ * (C) 2018 Alvise Spano' @ Universita' Ca' Foscari di Venezia
+ *)
+
+module SharpSolver.Impl
+
+open Absyn
+open Prelude
+open System
+
+let rationalize (x : float) : rational =    
+    let rec  aux (numero:float) (app:float) = if(numero*app%10.=0.) then rational(int(numero),int(app))
+                                                                         else aux (numero*10.) (app*10.)
+    in aux x 1.
+
+let monomial_degree (m : monomial) : int = match m with
+                                           Monomial(q,n) -> n
+let monomial_negate (m : monomial) : monomial = match m with
+                                                |Monomial(q,n) -> Monomial(-q,n)
+
+let polynomial_degree (p : polynomial) : int =     
+    let rec aux (gradoMassimo : int) (lst : polynomial) = match lst with
+                                                          Polynomial([]) -> gradoMassimo
+                                                          |Polynomial((Monomial(coeff,exp)) :: xs) -> if exp > gradoMassimo then aux exp (Polynomial(xs))
+                                                                                                                else aux gradoMassimo (Polynomial(xs))
+    in aux 0 p
+let polynomial_negate (p : polynomial) : polynomial = raise (NotImplementedException ())
+let normalized_polynomial_degree (np : normalized_polynomial) : int = raise (NotImplementedException ())
+
+let normalize (p : polynomial) : normalized_polynomial = raise (NotImplementedException ())
+let derive (p : polynomial) : polynomial = raise (NotImplementedException ())
+let reduce (e : expr) : polynomial = raise (NotImplementedException ())
+
+let solve0 (np : normalized_polynomial) : bool = raise (NotImplementedException ())
+let solve1 (np : normalized_polynomial) : rational = raise (NotImplementedException ())
+let solve2 (np : normalized_polynomial) : (float * float option) option = raise (NotImplementedException ())
+
+
+
+
